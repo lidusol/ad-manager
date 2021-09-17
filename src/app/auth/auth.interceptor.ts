1580bf4c58acc843689d241e9abbbebbec1a29b3
Lydia Solomon <lydia@etmsoftwareplc.com>
2021-09-17 15:22:45 +0300

    feat: add auth interceptors

---
+++ src/app/auth/auth.interceptor.ts
@@ -0,0 +1,19 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FACEBOOK_ACCESS_TOKEN } from 'src/environments/environment'
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    req = req.clone({
+      setHeaders: {
+        'Content-Type' : 'application/json; charset=utf-8',
+        'Accept'       : 'application/json',
+        'Authorization': `Bearer ${FACEBOOK_ACCESS_TOKEN}`,
+      },
+    });
+
+    return next.handle(req);
+  }
+}
