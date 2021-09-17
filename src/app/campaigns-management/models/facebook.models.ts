b80f74def57ae5e3152d6fa36cd15a3b6e19a432
Lydia Solomon <lydia@etmsoftwareplc.com>
2021-09-17 15:23:28 +0300

    feat: update model and add campaign service

--- src/app/campaigns-management/models/facebook.models.ts
+++ src/app/campaigns-management/models/facebook.models.ts
@@ -1,14 +1,37 @@
-import { LOCATION } from 'src/app/utils/data';
+export interface Campaign {
+    name: string;
+    status: string;
+    objective: string;
+    special_ad_categories: string[];
+    campaign_id?: string,
+    adsets?: any[]
+}
 
-export interface Facebook {
-    id?: string;
-    id_campagne?: number;
-    name?: string;
-    status?: string;
-    startDate?: string;
-    endDate?: string;
-    targetedLocations?: LOCATION[];
-    excludedLocations?: LOCATION[];
-    numberOfDays?: number;
-    budgetId?: number;
+export interface Adset {
+    name: string;
+    optimization_goal: string;
+    billing_event: string;
+    bid_amount: number
+    daily_budget: number
+    campaign_id?: string;
+    targeting: any;
+    start_time: string;
+    status: string;
+    adset_id?: string;
+    instagram_actor_id?: string;
+}
+
+export interface Creative {
+    name: string;
+    ad_format: string;
+    object_story_spec: any;
+    creative_id?: string
+}
+
+export interface Ad {
+    name: string;
+    adset_id: string;
+    creative: any;
+    status: string;
+    ad_id?: string;
 }
--- src/app/campaigns-management/services/facebook.service.ts
+++ src/app/campaigns-management/services/facebook.service.ts
@@ -1,28 +1,59 @@
-import { Injectable } from '@angular/core';
-import { Facebook } from '../models/facebook.models';
-import { map } from 'rxjs/operators';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core'
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http'
+import { Observable, throwError } from 'rxjs'
+import { catchError, retry, map } from 'rxjs/operators'
 
+import { Campaign, Adset, Ad } from '../models/facebook.models'
+import { ADAFRI_BUSINESS_ID } from 'src/environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class FacebookService {
+  apiUrl: string = 'http://localhost:7100'
+  fbGraphUrl: string = 'https://graph.facebook.com/v11.0'
 
-  constructor(private http: HttpClient, public afs: AngularFirestore,) { }
+  constructor(private http: HttpClient) { }
 
-  public getListCampaign(uid: string, accountId: string) {
-    return this.afs.collection<Facebook>('adwords-facebook', (ref) => ref.where('owner', '==', uid).where('accountId', '==', accountId).where('isArchived', '==', false).where('isExpress', '==', true)).snapshotChanges().pipe(
-      map((actions) => {
-        return actions.map((a) => {
-          const data = a.payload.doc.data();
-
-          return { id: a.payload.doc.id, ...data };
-        });
-      })
-    );
-
+  public createCampaign(campaign: Campaign): Observable<Campaign> {
+    return this.http.post<Campaign>(`${this.apiUrl}/fb-campaign`, campaign).pipe(
+      retry(1),
+      catchError((e) => throwError(e))
+    )
   }
 
+  public getCampaigns() {
+    return this.http.get<any>(`${this.apiUrl}/fb-campaign`).pipe(
+      retry(1),
+        catchError((e) => throwError(e))
+    )
+  }
+
+  public getCampaignById(id: string){
+    return this.http.get<Campaign>(`${this.apiUrl}/fb-campaign/${id}`).pipe(
+      retry(1),
+        catchError((e) => throwError(e))
+    )
+  }
+
+  public createAdset(adset: Adset): Observable<Adset>{
+    return this.http.post<Adset>(`${this.apiUrl}/fb-adset`, adset).pipe(
+      retry(1),
+        catchError((e) => throwError(e))
+    )
+  }
+
+  public getAdsets(){
+    return this.http.get<any>(`${this.apiUrl}/fb-adset`).pipe(
+      retry(1),
+        catchError((e) => throwError(e))
+    )
+  }
+
+  public getClientPages(){
+    return this.http.get<any>(`${this.fbGraphUrl}/${ADAFRI_BUSINESS_ID}/client_pages`).pipe(
+      retry(1),
+        catchError((e) => throwError(e))
+    )
+  }
 }
