7602639104d368ac3767995c7fa7897a9e137d69
Lydia Solomon <lydia@etmsoftwareplc.com>
2021-09-21 10:22:16 +0300

    feat: add creative and ad services

--- src/app/campaigns-management/services/facebook.service.ts
+++ src/app/campaigns-management/services/facebook.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core'
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http'
+import { HttpClient, HttpParams } from '@angular/common/http'
 import { Observable, throwError } from 'rxjs'
-import { catchError, retry, map } from 'rxjs/operators'
+import { catchError, retry } from 'rxjs/operators'
 
-import { Campaign, Adset, Ad } from '../models/facebook.models'
-import { ADAFRI_BUSINESS_ID } from 'src/environments/environment'
+import { Campaign, Adset, Creative, Ad } from '../models/facebook.models'
+import { ADAFRI_BUSINESS_ID, FACEBOOK_ACCESS_TOKEN, AD_ACCOUNT_ID } from 'src/environments/environment'
 
 @Injectable({
   providedIn: 'root'
@@ -25,35 +25,125 @@ export class FacebookService {
   public getCampaigns() {
     return this.http.get<any>(`${this.apiUrl}/fb-campaign`).pipe(
       retry(1),
-        catchError((e) => throwError(e))
+      catchError((e) => throwError(e))
     )
   }
 
   public getCampaignById(id: string){
     return this.http.get<Campaign>(`${this.apiUrl}/fb-campaign/${id}`).pipe(
       retry(1),
-        catchError((e) => throwError(e))
+      catchError((e) => throwError(e))
+    )
+  }
+
+  public deleteCampaign(id: string){
+    return this.http.delete<Campaign>(`${this.apiUrl}/fb-campaign/${id}`).pipe(
+      retry(1),
+      catchError((e) => throwError(e))
     )
   }
 
   public createAdset(adset: Adset): Observable<Adset>{
     return this.http.post<Adset>(`${this.apiUrl}/fb-adset`, adset).pipe(
       retry(1),
-        catchError((e) => throwError(e))
+      catchError((e) => throwError(e))
     )
   }
 
   public getAdsets(){
     return this.http.get<any>(`${this.apiUrl}/fb-adset`).pipe(
       retry(1),
-        catchError((e) => throwError(e))
+      catchError((e) => throwError(e))
+    )
+  }
+
+  public getAdsetById(id: string){
+    return this.http.get<Adset>(`${this.apiUrl}/fb-adset/${id}`).pipe(
+      retry(1),
+      catchError((e) => throwError(e))
+    )
+  }
+
+  public deleteAdset(id: string){
+    return this.http.delete<Adset>(`${this.apiUrl}/fb-adset/${id}`).pipe(
+      retry(1),
+      catchError((e) => throwError(e))
     )
   }
 
   public getClientPages(){
     return this.http.get<any>(`${this.fbGraphUrl}/${ADAFRI_BUSINESS_ID}/client_pages`).pipe(
       retry(1),
-        catchError((e) => throwError(e))
+      catchError((e) => throwError(e))
+    )
+  }
+
+  public getPageAccessToken(fbPageId: string){
+    const params = new HttpParams()
+     .set('fields', 'access_token')
+     .set('access_token', FACEBOOK_ACCESS_TOKEN)
+    return this.http.get<any>(`${this.fbGraphUrl}/${fbPageId}`, { params: params }).pipe(
+      retry(1),
+      catchError((e) => throwError(e))
+    )
+  }
+
+  public getConnectedInstagramAccount(fbPageId: string, accessToken: string){
+    const params = new HttpParams()
+      .set('access_token', accessToken)
+      .set('fields', 'id, username, profile_pic')
+    return this.http.get<any>(`${this.fbGraphUrl}/${fbPageId}/instagram_accounts`, { params: params }).pipe(
+      retry(1),
+      catchError((e) => throwError(e))
+    )
+  }
+
+  public uploadImage(imgData: FormData){
+    return this.http.post<any>(`${this.fbGraphUrl}/${AD_ACCOUNT_ID}/adimages`, imgData).pipe(
+      retry(1),
+      catchError((e) => throwError(e))
+    )
+  }
+
+  public createCreative(creative: Creative){
+    return this.http.post<Creative>(`${this.apiUrl}/fb-creative`, creative).pipe(
+      retry(1),
+      catchError((e) => throwError(e))
+    )
+  }
+
+  public getCreative(){
+    return this.http.get<any>(`${this.apiUrl}/fb-creative`).pipe(
+      retry(1),
+      catchError((e) => throwError(e))
+    )
+  }
+
+  public createAd(ad: Ad){
+    return this.http.post<Ad>(`${this.apiUrl}/fb-ad`, ad).pipe(
+      retry(1),
+      catchError((e) => throwError(e))
+    )
+  }
+
+  public getAds(){
+    return this.http.get<any>(`${this.http}/fb-ad`).pipe(
+      retry(1),
+      catchError((e) => throwError(e))
+    )
+  }
+
+  public getAdById(id: string){
+    return this.http.get<any>(`${this.http}/fb-ad/${id}`).pipe(
+      retry(1),
+      catchError((e) => throwError(e))
+    )
+  }
+
+  public deleteAd(id: string){
+    return this.http.delete<Ad>(`${this.apiUrl}/fb-ad/${id}`).pipe(
+      retry(1),
+      catchError((e) => throwError(e))
     )
   }
 }
